fdcb3ce660fe2e2bd29596f8083daa58575390a4
waylon <cn_wlei@163.com>
2022-11-14 12:52:30 +0800

    2

--- address_check/address_check.go
+++ address_check/address_check.go
@@ -14,8 +14,8 @@ import (
 func main() {
 	re := regexp.MustCompile("^0x[0-9a-fA-F]{40}$")
 
-	fmt.Printf("is valid: %v\n", re.MatchString("0x323b5d4c32345ced77393b3530b1eed0f346429d"))  // is valid: true
-	fmt.Printf("is  valid: %v\n", re.MatchString("0xZYXb5d4c32345ced77393b3530b1eed0f346429d")) // is valid: false
+	fmt.Printf("is valid: %v\n", re.MatchString("0x323b5d4c32345ced77393b3530b1eed0f346429d"))   // is valid: true
+	fmt.Printf("is   valid: %v\n", re.MatchString("0xZYXb5d4c32345ced77393b3530b1eed0f346429d")) // is valid: false
 
 	client, err := ethclient.Dial("xxx")
 	if err != nil {
